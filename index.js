8719c3a91599449d5985209a354d7ecc56b973b4
Natan <65575398+natanael163@users.noreply.github.com>
2020-12-09 14:09:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,22 +11,30 @@ class ContaCorrente{
 
     sacar (valor){
         if(this.saldo >=valor){
-            this.saldo -= valor;
+            this.saldo -= valor
+            return valor;
         }    
     }
 
+
+    depositar(valor){
+        if(valor <= 0)
+        {
+            return;
+        }
+
+        this.saldo += valor;
+    }
 }
 
 
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
 
-
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
@@ -35,18 +43,15 @@ contaCorrenteRicardo.saldo = 0;
 contaCorrenteRicardo.agencia = 1001;
 
 
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.saldo = 100;
-console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
 contaCorrenteRicardo.sacar(50);
 
-
-contaCorrenteRicardo.saldo -= valorSacado;
-
-
-console.log(contaCorrenteRicardo.saldo);
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
 
 
 
-console.log(cliente1);
-console.log(cliente2);
+
+console.log(contaCorrenteRicardo);
